8d3a790e191b77bcba4d0311402e7ff68c3d187d
Bartosz Stachowiak <sbartekt@op.pl>
2021-11-26 19:58:03 +0100

    Two companies test

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ from sim.simulation import Simulation
 
 
 def main():
-    """super_folder = "DS4\\"
-    folders = ["10_investors\\", "50_investors\\", "100_investors\\"]
-    investor_sizes = [10, 50, 100]
-    tries = 4
-    for folder, size in zip(folders, investor_sizes):
-        for i in range(tries):
-            simulation = Simulation(size, 20, super_folder + folder + f"prices{i+1}.txt")
-            simulation.run(720)
-            simulation.export()"""
     simulation = Simulation(20, 10)
     simulation.run(365)
 
